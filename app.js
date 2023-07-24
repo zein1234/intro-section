6e3f4b8072ca912987c1278cc87a64b95cc4a92d
zain srio <zein.srio@gmail.com>
2023-07-24 22:19:03 +0200

    assigned js menu function

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const menuBtn = document.querySelector(".mobile-menu-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const overflow = document.querySelector(".overflow");
 const mobileLinks = document.querySelectorAll(".mobile-links li");
+
+
+menuBtn.addEventListener("click", () => {
+  const icon = document.querySelector(".mobile-menu-btn img");
+  menuBtn.classList.toggle("active-btn");
+  mobileMenu.classList.toggle("active-menu");
+  overflow.classList.toggle("active-menu");
+  if (menuBtn.classList.contains("active-btn")) {
+    icon.src = "./images/icon-close-menu.svg";
+  } else {
+    icon.src = "./images/icon-menu.svg";
+  }
+});
