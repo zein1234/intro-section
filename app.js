fd8b25eeb0f4507c0653d254522284d3366b3c44
zain srio <zein.srio@gmail.com>
2023-07-24 22:18:06 +0200

    assigned js variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const navLinks = document.querySelectorAll(".nav-links li");
+const menuBtn = document.querySelector(".mobile-menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const overflow = document.querySelector(".overflow");
+const mobileLinks = document.querySelectorAll(".mobile-links li");
